a8b18eadb5b008132e420a0561b3260db71ed4df
SerenePawn <serenepawn@gmail.com>
2025-03-25 08:52:32 +0300

    transition to apscheduler

--- core/runtime.py
+++ core/runtime.py
@@ -1,29 +1,12 @@
 import asyncio
-import threading
-import schedule
-from asyncio import AbstractEventLoop
-from datetime import time
-from sqlite3 import Cursor, Row
 
+from apscheduler.schedulers.background import BackgroundScheduler
 from loguru import logger
 
 from core.app_state import AppState
 from core.background import send_attendances, parse_ocaps
 from core.db.db import init
-from core.misc import run_timer, run_timer_schedule
 from core.startup_commands import StartupParams, STARTUP_PARAMS
-
-
-def bg_attendances(state: AppState, loop: AbstractEventLoop):
-    asyncio.run(run_timer(state, time(10), send_attendances, loop))
-
-
-def bg_ocaps(state: AppState, loop: AbstractEventLoop):
-    asyncio.run(run_timer(state, 60, parse_ocaps, loop))
-
-
-def bg_schedules(state: AppState):
-    asyncio.run(run_timer_schedule(state, 60, schedule.run_pending))
 
 
 async def startup_app(params: list[str]):
@@ -39,21 +22,10 @@ async def startup_app(params: list[str]):
     logger.info("Init bg tasks")
     bg_loop = asyncio.get_event_loop()
 
-    # state.background_tasks_threads.extend((
-    #     threading.Thread(target=bg_ocaps, args=(state, bg_loop), daemon=True),
-    #     threading.Thread(target=bg_attendances, args=(state, bg_loop), daemon=True),
-    # ))
-    # for thread in state.background_tasks_threads:
-    #     thread.start()
-
-    schedule.every().minute.at(":01").do(lambda: bg_loop.create_task(parse_ocaps(state, bg_loop)))
-    schedule.every().day.at("12:00").do(lambda: bg_loop.create_task(send_attendances(state, bg_loop)))
-
-    state.background_tasks_threads.extend((
-        threading.Thread(target=bg_schedules, args=(state,), daemon=True),
-    ))
-    for thread in state.background_tasks_threads:
-        thread.start()
+    scheduler = BackgroundScheduler(timezone="Europe/Moscow")
+    scheduler.add_job(lambda: bg_loop.create_task(parse_ocaps(state, bg_loop)), "cron", second=0)
+    scheduler.add_job(lambda: bg_loop.create_task(send_attendances(state, bg_loop)), "cron", hour=12, minute=0)
+    scheduler.start()
 
     logger.info("Startup complete; Start TG polling")
     await state.dispatcher.start_polling(state.bot)
